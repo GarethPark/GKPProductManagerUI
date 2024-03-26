bcfc2f4e279f8aa78913989ab814cff1f5827f58
Gareth Park <garethpark@yahoo.com>
2024-03-26 18:44:04 +0000

    added params to product details

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,3 +1,7 @@
-export default function ProductDetail() {
-    return <h1>Details about the product</h1>;
+export default function ProductDetail({
+    params,
+  }: {
+    params: { productId: string };
+  }) {
+    return <h1>Details about product {params.productId}</h1>;
   }
