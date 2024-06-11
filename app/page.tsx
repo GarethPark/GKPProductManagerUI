435fe9deaa09d881d8f2634b18a097d9a72e4dbd
Gareth Park <garethpark@yahoo.com>
2024-06-11 07:55:03 +0100

    added homepage

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home(){
+    return (
+        <>
+        <div className="container mx-auto px-4">
+            <h1>Hello Product Managment</h1>
+            <p>test content</p>
+            <div className="w-full h-screen bg-blue-700"></div>
+            <p>
+                Some test text 
+            </p>
+        </div>
+        </>
+    );
+}
