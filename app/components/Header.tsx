6494582a372591fd87e5860cc7cfd9fc4ca626e6
Gareth Park <garethpark@yahoo.com>
2024-06-11 22:09:13 +0100

    added header comp

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,16 @@
-export const Header:React.FC = () => {
-    return<div>Gaff Header</div>
+
+'use client'
+
+export const Header = () => {
+    return(
+        <header>
+            <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-800">
+                <div className="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl">
+                <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                    
+                </ul>
+                </div>
+            </nav>
+        </header>
+    )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/navigation"
+import { Header } from "./components/Header"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navigation/>
+        <Header/>
         {children}
       </body>
     </html>
