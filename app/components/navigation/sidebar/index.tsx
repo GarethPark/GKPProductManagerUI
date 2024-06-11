21e3e80158d8379d404d4c4275c6bbf47c8ae5f5
Gareth Park <garethpark@yahoo.com>
2024-06-11 21:33:27 +0100

    added sidebar component

---
+++ app/components/navigation/sidebar/index.tsx
@@ -0,0 +1,18 @@
+
+
+
+const SideBar = ({
+    isOpen,
+    toggle,
+} : {
+    isOpen : Boolean;
+    toggle: () => void;
+}): JSX.Element => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default SideBar;
