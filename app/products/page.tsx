6c3771cbe12b08e9e27853861b389c58735cba88
Gareth Park <garethpark@yahoo.com>
2024-06-17 22:36:54 +0100

    centered list

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,14 +4,22 @@ import Link from "next/link";
 export default function ProductList() {
     return (
       <>
-
-      <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-          <li className="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600"><Link href={`/products/${1}`}>Go to Product 1</Link></li>
-          <li className="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600"><Link href={`/products/${1}`}>Go to Product 2</Link></li>
-          <li className="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600"><Link href={`/products/${1}`}>Go to Product 3</Link></li>
-          <li className="w-full px-4 py-2 rounded-b-lg"><Link href={`/products/${1}`}>Go to Product 4</Link></li>
-      </ul>
-
+      <div className="flex justify-center items-center min-h-screen">
+        <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+            <li className="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+              <Link href={`/products/${1}`}>Go to Product 1</Link>
+            </li>
+            <li className="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+              <Link href={`/products/${1}`}>Go to Product 2</Link>
+            </li>
+            <li className="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+              <Link href={`/products/${1}`}>Go to Product 3</Link>
+            </li>
+            <li className="w-full px-4 py-2 rounded-b-lg">
+              <Link href={`/products/${1}`}>Go to Product 4</Link>
+            </li>
+        </ul>
+      </div>
       </>
     );
   }
