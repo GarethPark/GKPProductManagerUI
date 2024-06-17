f8450f76e10a9ad24be19738f544036f89d6014d
Gareth Park <garethpark@yahoo.com>
2024-06-17 22:28:49 +0100

    fixed params issue

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,12 +1,15 @@
-
 import React from "react";
 
-const ProductDetails = ({ params }: { params: { productID: string } }) => {
-  const { productID } = params;
+const ProductDetails = ({ params }: { params: { productId: string } }) => {
+  console.log('params = ', JSON.stringify(params)); 
+
+  const { productId } = params; 
+
+  console.log('test log ' + productId) 
   return (
     <div className="bg-slate-500 w-96 text-center my-20 p-10 mx-20">
       <h1>
-        The Product ID is <b>{productID}</b>   
+        The Product ID is <b>{productId}</b>  
       </h1>
     </div>
   );
