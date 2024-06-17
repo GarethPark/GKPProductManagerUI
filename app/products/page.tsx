1700f8d4553940e0ddfbdac2e3c21f41b233280e
Gareth Park <garethpark@yahoo.com>
2024-06-17 22:33:30 +0100

    added list

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,15 +4,14 @@ import Link from "next/link";
 export default function ProductList() {
     return (
       <>
-        <h1>Product List</h1>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
 
-        
+      <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+          <li className="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600"><Link href={`/products/${1}`}>Go to Product 1</Link></li>
+          <li className="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600"><Link href={`/products/${1}`}>Go to Product 2</Link></li>
+          <li className="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600"><Link href={`/products/${1}`}>Go to Product 3</Link></li>
+          <li className="w-full px-4 py-2 rounded-b-lg"><Link href={`/products/${1}`}>Go to Product 4</Link></li>
+      </ul>
 
-        <Link href={`/products/${1}`}>Go to Product 1</Link>
-        <Link href={`/products/${2}`}>Go to Product 2</Link>
       </>
     );
   }
