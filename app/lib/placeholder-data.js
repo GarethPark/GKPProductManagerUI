8fe2d01e17382e2d640b39b72f0a8e28afc39cfa
Gareth Park <garethpark@yahoo.com>
2024-06-22 11:19:16 +0100

    added comment

---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,28 @@
+//instead use mockAPI?
+
+const products = [
+    {
+        id: 1,
+        name: "Apple iMac 2",
+        category: "PC",
+        brand: "Apple",
+        description: "A powerful computer with excellent graphics.",
+        price: 2999,
+      },
+      {
+          id: 2,
+          name: "Apple iPhone 12",
+          category: "Phone",
+          brand: "Apple",
+          description: "A powerful phone with excellent graphics.",
+          price: 4000,
+      },
+      {
+          id: 3,
+          name: "Apple iPad 12",
+          category: "Tablet",
+          brand: "Apple",
+          description: "A powerful iPad.",
+          price: 10000,
+      }
+]
